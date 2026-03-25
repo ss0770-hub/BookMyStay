a8449364e645833a74ac3c1fdfd747ae0332e4c6
ss0770 <ss0770@srmist.edu.in>
2026-03-25 09:07:14 +0530

    UC1: Add welcome message module

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,2 +1,20 @@
+
 public class BookMyStay {
+
+    /**
+     * Main method - entry point of the application.
+     *
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("=====================================");
+        System.out.println(" Welcome to Book My Stay Application ");
+        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println("=====================================");
+
+        // Application ends here
+    }
 }
+
